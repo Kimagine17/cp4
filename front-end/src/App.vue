f32428269b81f0f2513c4130a38a6c468b5fb459
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-02 16:51:25 -0600

    added more files and changed front end

---
+++ front-end/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <!-- <div class="logo">
+        <img src="/ordinary.png">
+      </div> -->
+    </router-link>
+    <div class="title">
+      <h1>BOOKSY</h1>
+    </div>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="footer">
+    <p>Created by Hannah K. Nelson and Kimberly I. McKinnon!</p>
+    <p>Contact us at kimberlite17@gmail.com or hknelson@gmail.com</p>
+    <a href="https://github.com/Kimagine17/cp4/">Github</a>
+  </div>
+</div>
+</template>
