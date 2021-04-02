fe2b27856ee757864d9bf2f26175d6ac6ff95ec8
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-02 11:32:15 -0600

    Post a book review

--- back-end/server.js
+++ back-end/server.js
@@ -26,7 +26,6 @@ const bookSchema = new mongoose.Schema({
 //book model
 const Book = mongoose.model('Book', bookSchema);
 
-
 //Create a Book
 app.post('/api/books', async(req, res) => {
     const book = new Book ({
@@ -44,16 +43,61 @@ app.post('/api/books', async(req, res) => {
     }
 });
 
+//Get the books
+app.get('/api/books', async(req, res) => {
+    try {
+        let books = await Book.find();
+        res.send(books);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
 
-
-
-
+//All the review stuff
 const reviewSchema = new mongoose.Schema({
+    book: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Book'
+    },
     review: String,
-    person: String,
-    book: String,
+    person: String
     //TO-DO: photo: String,
 });
 
+//review model
+const Review = mongoose.model('Review', reviewSchema);
+
+app.post('/api/books/:bookID/reviews', async(req, res) => {
+    try {
+        let book = await Book.findOne({_id: req.params.bookID});
+        if(!book) {
+            res.send(404);
+            return;
+        }
+        let review = new Review ({
+            book: book,
+            review: req.body.review,
+            person: req.body.person,
+        //TO-DO: photo: String,
+        });
+        await review.save();
+        res.send(review);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+//Get the books
+// app.get('/api/books', async(req, res) => {
+//     try {
+//         let books = await Book.find();
+//         res.send(books);
+//     } catch (error) {
+//         console.log(error);
+//         res.sendStatus(500);
+//     }
+// });
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
