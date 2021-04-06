2a8d9794c25268d8bb65372cf915a7ee0d45d6f7
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-06 02:45:19 -0600

    Added VUE Multiselect feature, can post reviews

--- front-end/src/views/SubmitReview.vue
+++ front-end/src/views/SubmitReview.vue
@@ -2,37 +2,35 @@
   <div class="s-review">
     <h1>Submit a Review</h1>
     <div class="inputField-container">
-        <!-- <div class="inputField">
+        <div class="inputField">
             <div class="heading">
                 <h2>Create new user:</h2>
             </div>
             <div class="add">
                 <div class="form">
-                    <input v-model="name" placeholder="Name">
-                    <p></p>
-                    <input v-model="about" placeholder="About You">
-                    <p></p>
-                    <button @click="upload">Upload</button>
+                    <p>Choose a book:</p>
+                    <multiselect label="name" v-model="book" :options="books"></multiselect>
+                    <!-- <multiselect track-by="id" label="name" v-model="book" :options="books"></multiselect> -->
+                    <p>Who's writing the review?</p>
+                    <multiselect track-by="id" label="name" v-model="person" :options="users"></multiselect>
+                    <p>Write your review!</p>
+                    <input v-model="review" placeholder="review">
+                    <button @click="upload(book, person)">Upload</button>
                 </div>
                 <div class="upload" v-if="addItem">
                     <h2>{{addItem.title}}</h2>
                     <img :src="addItem.path" />
                 </div>
             </div>
-        </div>inputField -->
+        </div>inputField
         <div class="inputField">
             <div class="heading">
                 <h2>Edit/Delete a user:</h2>
             </div> <!--heading-->
             <div class="edit">
                 <div class="form">
-                    <select v-model="findName">
-                    <div class="suggestions" v-if="suggestions.length > 0">
-                        <div class="suggestion" v-for="s in suggestions" :key="s.id" @click="selectItem(s)">{{s.name}}
-                        </div>
-                    </div>
                 </div>
-                <div class="editing">
+                <!-- <div class="editing">
                     <div class="upload" v-if="findItem">
                         <p>Change Name:</p>
                         <input v-model="findItem.name">
@@ -45,7 +43,7 @@
                         <button class="action" @click="editItem(findItem)">Edit</button>
                         <button class="action" @click="deleteItem(findItem)">Delete</button>
                     </div>
-                </div> <!--editing-->
+                </div> editing -->
             </div><!--edit-->
         </div> <!--input field-->
     </div> <!--input field-container -->  </div>
@@ -53,80 +51,102 @@
 
 <script>
 import axios from 'axios';
+import Multiselect from 'vue-multiselect';
 export default {
-    name: 'SubmitPersons',
+    components: { Multiselect },
+    name: 'SubmitReviews',
     data() {
         return {
-            name: "",
-            about: "",
-            items: [],
-            findName: "",
+            book: null,
+            person: null,
+            books: [],
+            users: [],
+            // findName: "",
             findItem: null,
             addItem: null,
+            review: "",
         }
     },
-    computed: {
-        suggestions() {
-            let items = this.items.filter(item => item.name.toLowerCase().startsWith(this.findName.toLowerCase()));
-            return items.sort((a, b) => a.name > b.name);
-        }
-    },
+    // computed: {
+    // },
     created() {
-        this.getItems();
+        this.getBooks();
+        this.getUsers();
     },
     methods: {
         fileChanged(event) {
             this.file = event.target.files[0]
         },
-        async upload() {
+        async upload(book, person) {
             try {
-                let r1 = await axios.post('/api/persons', {
-                    name: this.name,
-                    about: this.about
+                let r1 = await axios.post("/api/books/" + book._id + "/persons/" + person._id + "/reviews", {
+                    book: this.book,
+                    person: this.person,
+                    review: this.review
                 });
                 this.addItem = r1.data;
-                this.name = "";
-                this.about = "";
+                this.review = "";
+                this.book = null;
+                this.person = null;
             } catch (error) {
                 console.log(error);
             }
         },
-        async getItems() {
+        // async getItems() {
+        //     try {
+        //         let response = await axios.get("/api/persons");
+        //         this.items = response.data;
+        //         return true;
+        //     } catch (error) {
+        //         console.log(error);
+        //     }
+        // },
+        async getBooks() {
+            try {
+                let response = await axios.get("/api/books");
+                this.books = response.data;
+                return true;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async getUsers() {
             try {
                 let response = await axios.get("/api/persons");
-                this.items = response.data;
+                this.users = response.data;
                 return true;
             } catch (error) {
                 console.log(error);
             }
         },
-        selectItem(item) {
-            this.findName = "";
-            this.findItem = item;
-        },
-        async deleteItem(item) {
-            try {
-                await axios.delete("/api/persons/" + item._id);
-                this.findItem = null;
-                this.getItems();
-                return true;
-            } catch (error) {
-                console.log(error);
-            }
-        },  
-        async editItem(item) {
-            try {
-                await axios.put("/api/persons/" + item._id, {
-                name: this.findItem.name,
-                about: this.findItem.about,
-                });
-                this.findItem = null;
-                this.getItems();
-                return true;
-            } catch (error) {
-                console.log(error);
-            }
-        },
+        // selectItem(item) {
+        //     this.findName = "";
+        //     this.findItem = item;
+        // },
+        // async deleteItem(item) {
+        //     try {
+        //         await axios.delete("/api/persons/" + item._id);
+        //         this.findItem = null;
+        //         this.getItems();
+        //         return true;
+        //     } catch (error) {
+        //         console.log(error);
+        //     }
+        // },  
+        // async editItem(item) {
+        //     try {
+        //         await axios.put("/api/persons/" + item._id, {
+        //         name: this.findItem.name,
+        //         about: this.findItem.about,
+        //         });
+        //         this.findItem = null;
+        //         this.getItems();
+        //         return true;
+        //     } catch (error) {
+        //         console.log(error);
+        //     }
+        // },
     }
 }
 </script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
