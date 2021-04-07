b5def15fd538c0a4dbe2a90b752f2d1459411639
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-06 19:51:58 -0600

    books displayed on the home page (poorly)

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,13 +1,81 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
-  </div>
+    <section class="image-gallery">
+      <div class="image" v-for="book in books" :key="book.id">
+        <h2>{{book.name}}</h2>
+        <h2>{{book.genre}}</h2>
+        <h2>{{book.genre.name}}</h2>
+        <h2>{{book.description}}</h2>
+        <img :src="book.photoPath" />
+      </div>
+    </section>
+  </div> <!--home-->
 </template>
 
-<style scooped>
-.home {
-    height:100%;
-    width: 90%;
-    margin: 20px auto;
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+export default {
+  name: 'Home',
+  data() {
+    return {
+     books: [],
+    }
+  },
+  created() {
+    this.getBooks();
+  },
+  methods: {
+    async getBooks() {
+      try {
+        let response = await axios.get("/api/books");
+        this.books = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+.image h2 {
+  font-style: italic;
+}
+/* Masonry */
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+.image-gallery {
+  column-gap: 1.5em;
+}
+.image {
+  margin: 0 0 1.5em;
+  display: inline-block;
+  width: 100%;
+}
+.image img {
+  width: 100%;
+}
+/* Masonry on large screens */
+@media only screen and (min-width: 1024px) {
+  .image-gallery {
+    column-count: 4;
+  }
+}
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .image-gallery {
+    column-count: 3;
+  }
+}
+/* Masonry on small screens */
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .image-gallery {
+    column-count: 2;
+  }
 }
 </style>
